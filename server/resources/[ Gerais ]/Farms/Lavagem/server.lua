f0ede64b7b1df42ea6eee78e123b50bde2aa50d0
Reborn-Studios <allysonbedim99@gmail.com>
2024-10-29 23:31:26 -0300

    update(Farms): lavagem

--- server/resources/[ Gerais ]/Farms/Lavagem/server.lua
+++ server/resources/[ Gerais ]/Farms/Lavagem/server.lua
@@ -1,39 +1,39 @@
 local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
-local Tools = module("vrp","lib/Tools")
 local Webhooks = module("Reborn/webhooks")
 vRP = Proxy.getInterface("vRP")
-vRPclient = Tunnel.getInterface("vRP")
-emP = {}
-Tunnel.bindInterface("Lavagem",emP)
+Lavagem = {}
+Tunnel.bindInterface("Lavagem",Lavagem)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- Checar Itens
 -----------------------------------------------------------------------------------------------------------------------------------------
-function emP.checkItens()
+function Lavagem.checkItens()
 	local source = source
 	local user_id = vRP.getUserId(source)
-	local dinheiro_sujo = vRP.getInventoryItemAmount(user_id,"dollars2")
 	if user_id then
+		local dinheiro_sujo = vRP.getInventoryItemAmount(user_id,"dollars2")
 		if vRP.hasPermission(user_id,Farms.lavagem.perm) then
 			if dinheiro_sujo >= Farms.lavagem['dinheiro_sujo'].min_money and dinheiro_sujo <= Farms.lavagem['dinheiro_sujo'].max_money then
-				return true 
+				return true
 			else
-				TriggerClientEvent("Notify",source,"negado","Dinheiro insuficiente.",4000)  
+				TriggerClientEvent("Notify",source,"negado","Dinheiro insuficiente.",4000)
 			end
+		else
+			TriggerClientEvent("Notify",source,"negado","Sem permissão.",4000)
 		end
 	end
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- Pagamento
 -----------------------------------------------------------------------------------------------------------------------------------------
-function emP.checkPayment()
+function Lavagem.checkPayment()
     local source = source
     local user_id = vRP.getUserId(source)
-	local identity = vRP.getUserIdentity(user_id)
-	local dinheiro = vRP.getInventoryItemAmount(user_id,"dollars2")
     if user_id then
+		local identity = vRP.getUserIdentity(user_id)
+		local dinheiro = vRP.getInventoryItemAmount(user_id,"dollars2")
 		if vRP.tryGetInventoryItem(user_id,"dollars2",dinheiro) then
-			local payment = dinheiro * (Farms.lavagem['dinheiro_sujo'].porcentagem)/100
+			local payment = dinheiro * (Farms.lavagem['dinheiro_sujo'].porcentagem) / 100
 			vRP.giveInventoryItem(user_id,"dollars",payment,true)
 			vRP.createWeebHook(Webhooks.webhooklavagem,"```prolog\n[PASSAPORTE]: "..user_id.." \n[NOME]: "..identity.name.." "..identity.name2.." \n[LAVOU]: "..dinheiro.." \n[RECEBEU]: "..payment.." "..os.date("\n[Data]: %d/%m/%y \n[Hora]: %H:%M:%S").." \r```")
 		end
