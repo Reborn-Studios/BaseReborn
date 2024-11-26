8111b0c5d58af5dfc7ddfd1498ade3417c9680db
Reborn-Studios <allysonbedim99@gmail.com>
2024-11-26 09:58:54 -0300

    update: ox_inventory

--- server/resources/[ Scripts ]/ox_inventory/server.lua
+++ server/resources/[ Scripts ]/ox_inventory/server.lua
@@ -70,13 +70,14 @@ exports('setPlayerInventory', server.setPlayerInventory)
 AddEventHandler('ox_inventory:setPlayerInventory', server.setPlayerInventory)
 
 ---@param playerPed number
----@param coordinates vector3|vector3[]
----@param distance? number
----@return vector3|false
-local function getClosestStashCoords(playerPed, coordinates, distance)
+---@param stash OxInventory
+---@return vector3?
+local function getClosestStashCoords(playerPed, stash)
 	local playerCoords = GetEntityCoords(playerPed)
+	local distance = stash.distance or 10
+    local coordinates = stash.coords
 
-	if not distance then distance = 10 end
+	if not coordinates then return end
 
 	if type(coordinates) == 'table' then
 		for i = 1, #coordinates do
@@ -87,10 +88,10 @@ local function getClosestStashCoords(playerPed, coordinates, distance)
 			end
 		end
 
-		return false
+		return
 	end
 
-	return #(coordinates - playerCoords) < distance and coordinates
+	return #(coordinates - playerCoords) < distance and coordinates or nil
 end
 
 ---@param source number
@@ -100,8 +101,10 @@ end
 ---@return table | false | nil, table | false | nil, string?
 local function openInventory(source, invType, data, ignoreSecurityChecks)
 	if Inventory.Lock then return false end
-	local left = Inventory(source) --[[@as OxInventory]]
+	local left = Inventory(source)
 	local right, closestCoords
+
+	if not left then return end
 
     left:closeInventory(true)
 	Inventory.CloseAll(left, source)
@@ -109,6 +112,8 @@ local function openInventory(source, invType, data, ignoreSecurityChecks)
     if invType == 'player' and data == source then
         data = nil
     end
+
+	local playerPed = left.player.ped
 
 	if data then
         local isDataTable = type(data) == 'table'
@@ -118,18 +123,43 @@ local function openInventory(source, invType, data, ignoreSecurityChecks)
 			if right == false then return false end
 		elseif isDataTable then
 			if data.netid then
+				local entity = NetworkGetEntityFromNetworkId(data.netid)
+
+                if not entity then return end
+
+                if not ignoreSecurityChecks then
+                    if #(GetEntityCoords(playerPed) - GetEntityCoords(entity)) > 16 then return end
+                end
+
+                if invType == 'glovebox' then
+                    if not ignoreSecurityChecks and GetVehiclePedIsIn(playerPed, false) ~= entity then
+                        return
+                    end
+
+                    if not data.id then
+                        data.id = 'glove'..GetVehicleNumberPlateText(entity)
+                    end
+                end
+
                 if invType == 'trunk' then
-                    local entity = NetworkGetEntityFromNetworkId(data.netid)
-                    local lockStatus = entity > 0 and GetVehicleDoorLockStatus(entity)
+                    local lockStatus = ignoreSecurityChecks and 0 or GetVehicleDoorLockStatus(entity)
 
                     -- 0: no lock; 1: unlocked; 8: boot unlocked
                     if lockStatus > 1 and lockStatus ~= 8 then
                         return false, false, 'vehicle_locked'
                     end
+
+					if not data.id  then
+                        data.id = 'trunk'..GetVehicleNumberPlateText(entity)
+                    end
                 end
 
 				data.type = invType
 				right = Inventory(data)
+
+				if right and data.netid ~= right.netid then
+                    return
+                end
 			elseif invType == 'drop' then
 				right = Inventory(data.id)
 			else
@@ -189,7 +219,7 @@ local function openInventory(source, invType, data, ignoreSecurityChecks)
 		end
 
 		if not ignoreSecurityChecks and right.coords then
-			closestCoords = getClosestStashCoords(left.player.ped, right.coords)
+			closestCoords = getClosestStashCoords(playerPed, right)
 
 			if not closestCoords then return end
 		end
