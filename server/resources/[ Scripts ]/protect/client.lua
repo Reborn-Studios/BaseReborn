629291efefd2082a186554e78fdfbd25c260200a
Reborn-Studios <allysonbedim99@gmail.com>
2024-12-27 15:08:46 -0300

    refactor(protect)

--- server/resources/[ Scripts ]/protect/client.lua
+++ server/resources/[ Scripts ]/protect/client.lua
@@ -1,22 +1,15 @@
 local Tunnel = module("vrp","lib/Tunnel")
-local Proxy = module("vrp","lib/Proxy")
-vRP = Proxy.getInterface("vRP")
-
-src = {}
-Tunnel.bindInterface("protect",src)
-Proxy.addInterface("protect",src)
-acClient = Tunnel.getInterface("protect")
+Server = Tunnel.getInterface("protect")
 
 RegisterNUICallback("loadNuis", function(data, cb)
-	acClient.pegaTrouxa()
+	Server.pegaTrouxa()
 end)
 
-Config = {}
-
-Config.DrawingTime = 60*1000 --1 MINUT
-Config.TextColor = {r=255, g=255,b=255} -- WHITE (Player Data)
-Config.AlertTextColor = {r=255, g=0, b=0} -- RED (Player Left Game)
 --------------------------- ANTI CL ----------------------------------
+Config = {}
+Config.DrawingTime = 60*1000                        -- 1 MINUTO
+Config.TextColor = { r = 255, g = 255, b = 255 }    -- BRANCO
+Config.AlertTextColor = { r = 255, g = 0, b = 0 }   -- VERMELHO
 
 local show3DText = false
 
@@ -37,12 +30,12 @@ end)
 function Display(id, crds, identifier, reason)
     local displaying = true
 
-    Citizen.CreateThread(function()
+    CreateThread(function()
         Wait(Config.DrawingTime)
         displaying = false
     end)
-	
-    Citizen.CreateThread(function()
+
+    CreateThread(function()
         while displaying do
             Wait(5)
             local pcoords = GetEntityCoords(PlayerPedId())
@@ -50,7 +43,7 @@ function Display(id, crds, identifier, reason)
                 DrawText3DSecond(crds.x, crds.y, crds.z+0.15, "Player saiu do jogo")
                 DrawText3D(crds.x, crds.y, crds.z, "ID: "..identifier.." \nMotivo: "..reason)
             else
-                Citizen.Wait(2000)
+                Wait(2000)
             end
         end
     end)
@@ -58,26 +51,24 @@ end
 
 function DrawText3DSecond(x,y,z, text)
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
     SetTextScale(0.45, 0.45)
     SetTextFont(4)
-    SetTextProportional(1)
+    SetTextProportional(true)
     SetTextColour(Config.AlertTextColor.r, Config.AlertTextColor.g, Config.AlertTextColor.b, 215)
     SetTextEntry("STRING")
-    SetTextCentre(1)
+    SetTextCentre(true)
     AddTextComponentString(text)
     DrawText(_x,_y)
 end
 
 function DrawText3D(x,y,z, text)
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
     SetTextScale(0.45, 0.45)
     SetTextFont(4)
-    SetTextProportional(1)
+    SetTextProportional(true)
     SetTextColour(Config.TextColor.r, Config.TextColor.g, Config.TextColor.b, 215)
     SetTextEntry("STRING")
-    SetTextCentre(1)
+    SetTextCentre(true)
     AddTextComponentString(text)
     DrawText(_x,_y)
 end
--- server/resources/[ Scripts ]/protect/server.lua
+++ server/resources/[ Scripts ]/protect/server.lua
@@ -1,29 +1,28 @@
-local Tunnel = module("vrp","lib/Tunnel")
-local Proxy = module("vrp","lib/Proxy")
-local Webhooks = module("Reborn/webhooks")
+local Proxy = module("vrp","lib/Proxy") or {}
+local Tunnel = module("vrp","lib/Tunnel") or {}
+local Webhooks = module("Reborn/webhooks") or {}
 vRP = Proxy.getInterface("vRP")
-vRPclient = Tunnel.getInterface("vRP", "protect")
 
-src = {}
-Tunnel.bindInterface("protect",src)
-Proxy.addInterface("protect",src)
+Protect = {}
+Tunnel.bindInterface("protect",Protect)
 
-local loaded = {}
-
-function src.pegaTrouxa()
+function Protect.pegaTrouxa()
     local source = source
     local user_id = vRP.getUserId(source)
     local identity = vRP.getUserIdentity(user_id)
-
     local fields2 = {}
-    table.insert(fields2, { name = "ChomeInspector:", value = 'ID => **'..user_id..'** \nFoi pego tentando roubar o Html/Client da cidade.', inline = true });
+    table.insert(fields2, {
+        name = "ChomeInspector:",
+        value = 'ID => **'..user_id..'** \nFoi pego tentando roubar o Html/Client da cidade.',
+        inline = true
+    });
     PerformHttpRequest("", function(err, text, headers) end, 'POST', json.encode({username = "In Game Log System", content = nil, embeds = {{color = 16754176, fields = fields2,}}}), { ['Content-Type'] = 'application/json' }) 
     print("Tentativa de Acesso ao Chrome Inspector! ID: "..user_id)
     vRP.execute("vRP/set_banned",{ steam = tostring(identity.steam), banned = 1 })
     vRP.kick(user_id,"Administração Mandou um Beijo <3")
 end
 
-RegisterCommand("combat", function(source, args, rawcmd)
+RegisterCommand("cl", function(source, args, rawcmd)
     TriggerClientEvent("pixel_antiCL:show", source)
 end)
 
