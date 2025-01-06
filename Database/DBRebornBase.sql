b6e7eb89011bf156631d50a3a21210f45bb6e9c9
Reborn-Studios <allysonbedim99@gmail.com>
2025-01-06 14:39:25 -0300

    remove(smartbank): data tables

--- Database/DBRebornBase.sql
+++ Database/DBRebornBase.sql
@@ -154,51 +154,6 @@ CREATE TABLE IF NOT EXISTS `will_ficha` (
 	PRIMARY KEY (`id`) USING BTREE
 ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `smartbank_accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(50) DEFAULT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `amount` double DEFAULT NULL,
-  UNIQUE KEY `id` (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
-CREATE TABLE IF NOT EXISTS `smartbank_cards` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(50) DEFAULT NULL,
-  `holder` varchar(50) DEFAULT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `number` varchar(50) DEFAULT NULL,
-  `pin` varchar(50) DEFAULT NULL,
-  `hold` int(11) DEFAULT 0,
-  `data` text DEFAULT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `smartbank_fines` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pix` varchar(50) NOT NULL DEFAULT '0',
-  `from` varchar(50) NOT NULL DEFAULT '0',
-  `source` varchar(50) NOT NULL DEFAULT '0',
-  `author` varchar(50) NOT NULL DEFAULT '0',
-  `amount` double NOT NULL DEFAULT 0,
-  `reason` varchar(50) NOT NULL DEFAULT '0',
-  `time` int(255) NOT NULL,
-  `active` int(1) NOT NULL DEFAULT 1,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `smartbank_statements` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pix` varchar(50) NOT NULL DEFAULT '0',
-  `from` varchar(50) NOT NULL DEFAULT '0',
-  `source` varchar(50) NOT NULL DEFAULT '0',
-  `type` varchar(50) NOT NULL DEFAULT '0',
-  `amount` double NOT NULL DEFAULT 0,
-  `reason` varchar(50) NOT NULL DEFAULT '0',
-  `time` int(255) DEFAULT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS `user_bans` (
 	`name` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`license` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
