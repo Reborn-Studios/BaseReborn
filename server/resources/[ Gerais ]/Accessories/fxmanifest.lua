f19b7442ea903f7e3c4c8aac03bb8ff2ece243c0
Reborn-Studios <allysonbedim99@gmail.com>
2025-03-21 23:17:20 -0300

    feat: showme script

--- server/resources/[ Gerais ]/Accessories/fxmanifest.lua
+++ server/resources/[ Gerais ]/Accessories/fxmanifest.lua
@@ -1,28 +1,20 @@
 fx_version "bodacious"
 game "gta5"
 
+ui_page "web-side/index.html"
+
 client_scripts {
 	"@vrp/lib/utils.lua",
-	"Academy/client-side/*",
-	"ArmBraker/client-side/*",
-	"Blipsystem/client-side/*",
-	"Pets/client-side/*",
-	"Rope/client-side/*",
-	"Safelocker/client-side/*",
-	"Skate/client-side/*",
-	"Anims/client-side/*",
-	"Tackle/client-side/*",
+	"**/client-side/*",
 }
 
 server_scripts {
 	"@vrp/lib/utils.lua",
-	"ArmBraker/server-side/*",
-	"Blipsystem/server-side/*",
-	"Pets/server-side/*",
-	"Rope/server-side/*",
-	"Safelocker/server-side/*",
-	"Skate/server-side/*",
-	"Tackle/server-side/*",
+	"**/server-side/*",
 }
 
-file "Skate/stream/p_defilied_ragdoll_01_s.ydr"
+files {
+	"stream/p_defilied_ragdoll_01_s.ydr",
+	"web-side/*",
+	"web-side/**/*"
+}
