dded2ec5cb0354a22299804e645818b96f5fed91
Reborn-Studios <allysonbedim99@gmail.com>
2025-04-04 16:21:12 -0300

    update(racing): sql collate

--- server/resources/[ QBCore ]/gfx-racing/server/main.lua
+++ server/resources/[ QBCore ]/gfx-racing/server/main.lua
@@ -6,31 +6,31 @@ Citizen.CreateThread(function()
     GFX.Framework = GFX.GetFramework()
     MySQL.query([[
         CREATE TABLE IF NOT EXISTS `gfx_racing` (
-            `identifier` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-            `routes` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-            `racehistory` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `identifier` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+            `routes` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+            `racehistory` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
             `win` INT(11) NULL DEFAULT NULL,
             `lose` INT(11) NULL DEFAULT NULL,
-            `favouritecar` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `favouritecar` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
             `distance` BIGINT(20) NULL DEFAULT NULL,
-            `charname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `charname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
             `incomingrace` INT(11) NULL DEFAULT NULL,
-            `lastrace` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-            `playerphoto` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci'
-        ) COLLATE='utf8mb3_general_ci' ENGINE=InnoDB;
+            `lastrace` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+            `playerphoto` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci'
+        ) COLLATE='utf8_general_ci' ENGINE=InnoDB;
     ]])
     MySQL.query([[
         CREATE TABLE IF NOT EXISTS `races` (
-            `owner` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-            `name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `owner` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+            `name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
             `reward` INT(11) NULL DEFAULT NULL,
-            `date` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `date` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
             `maxplayers` INT(11) NULL DEFAULT NULL,
-            `route` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+            `route` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
             `id` BIGINT(20) NULL DEFAULT NULL,
-            `players` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-            `luadate` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci'
-        ) COLLATE='utf8mb3_general_ci' ENGINE=InnoDB;
+            `players` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+            `luadate` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci'
+        ) COLLATE='utf8_general_ci' ENGINE=InnoDB;
     ]])
     Wait(2000)
     local races = MySQL.query.await('SELECT * FROM races')
