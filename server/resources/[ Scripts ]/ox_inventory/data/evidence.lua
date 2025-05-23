259b310a30f03da9233288ac05ecc733121e9746
Reborn-Studios <allysonbedim99@gmail.com>
2025-05-22 23:47:25 -0300

    update: bau de evidencias para policia

--- server/resources/[ Scripts ]/ox_inventory/data/evidence.lua
+++ server/resources/[ Scripts ]/ox_inventory/data/evidence.lua
@@ -1,14 +1,14 @@
 return {
-	--[[ {
-		coords = vec3(458.97, -982.79, 30.68),
+	{
+		coords = vec3(89.54,-417.91,47.5),
 		target = { -- qtarget support
-			name = 'mrpd_evidence', -- name of zone must be uniuqe
-			loc = vec3(459.07, -984.07, 30.69),
+			name = 'lspd_evidence', -- name of zone must be uniuqe
+			loc = vec3(89.44,-418.25,46.82),
 			length = 1.4,
 			width = 3.2,
 			heading = 0,
-			minZ = 29.09,
-			maxZ = 31.89
+			minZ = 45.09,
+			maxZ = 47.09
 		}
-	} ]]
+	}
 }
