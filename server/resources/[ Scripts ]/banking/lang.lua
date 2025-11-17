d8b54c1fc7093b2376a64341bd1393efade0eb84
Reborn-Studios <allysonbedim99@gmail.com>
2025-11-17 16:01:29 -0300

    feat: novo banco

---
+++ server/resources/[ Scripts ]/banking/lang.lua
@@ -0,0 +1,158 @@
+Lang = {
+    client = {
+        bank_interact = '[E] - Banco',
+        atm_interact = '[E] - Caixa Eletrônico',
+        cool_down = 'Você deve aguardar um momento antes de tentar novamente',
+        myself_not_found = 'Não foi possível encontrar seus dados, tente novamente mais tarde',
+        no_target = 'Nenhum alvo encontrado',
+        no_money = 'Você não tem dinheiro suficiente para esta transação',
+        min_amount = 'Esta ação exige um valor mínimo de $1',
+        transfer_success = 'Transferência realizada com sucesso',
+        cant_transfer_to_self = 'Você não pode transferir dinheiro para si mesmo',
+        withdraw_success = 'Saque realizado com sucesso',
+        deposit_success = 'Depósito realizado com sucesso',
+        invalid_data = 'Dados inválidos enviados pelo cliente',
+        uncaught_error = 'Ocorreu um erro inesperado, entre em contato em discord.gg/svdden',
+    },
+    UI = {
+        currency = '$',
+        navbar = {
+            account_overview_text = 'Visão Geral',
+            accounts_labels = {
+                home = 'Início',
+                personal = 'Pessoal',
+                savings = 'Poupança',
+            },
+            creditscore = {
+                title = 'Pontuação de Crédito',
+                innertitle = 'Crédito',
+                creditscoresubtext = 'Verifique regularmente para monitorar e melhorar.',
+                low = 'Baixa',
+                good = 'Boa',
+                high = 'Alta',
+            },
+            account_text = 'Sua Conta',
+            logout = 'Sair',
+        },
+        home_page = {
+            card_section = {
+                title = 'Seu Cartão',
+                subtext = 'Gerencie seu cartão e acompanhe seus gastos rapidamente.',
+                cardNumber = 'Número do Cartão',
+                date = 'Validade',
+                cvv = 'CVV',
+            },
+            quick_transfer = {
+                title = 'Transferência Rápida',
+                subtext = 'Envie dinheiro instantaneamente com apenas alguns cliques.',
+                transferOptions = {
+                    {
+                        text1 = 'Destinatário',
+                        text2 = 'Digite um ID',
+                    },
+                    {
+                        text1 = 'Valor',
+                    },
+                    {
+                        text1 = 'Recentes',
+                    }
+                }
+            },
+            spending = {
+                title = 'Seus Gastos',
+                subtext = 'Acompanhe seus gastos diários e mantenha o controle financeiro.',
+                daily = 'Diário',
+            },
+            transactons = {
+                title = 'Transações',
+                types = {
+                    text1 = 'Tipo',
+                    text2 = 'Valor',
+                    text3 = 'Status',
+                    text4 = 'Data',
+                }
+            }
+        },
+        personal_page = {
+            title = 'Informações Principais',
+            subtext = 'Informações da sua conta e ações disponíveis.',
+            personal_info = {
+                user_header = 'Conta',
+                cardType = 'Pessoal',
+                cardNumber = 'Número do Cartão',
+                date = 'Validade',
+                cvv = 'CVV',
+            },
+            transactions = {
+                title = 'Transações',
+                types = {
+                    text1 = 'Tipo',
+                    text2 = 'Valor',
+                    text3 = 'Status',
+                    text4 = 'Data',
+                }
+            },
+            actions = {
+                title = 'Ações',
+                subtext = 'Ações rápidas para gerenciar sua conta.',
+                menus = {
+                    headers = {
+                        'Sacar',
+                        'Depositar',
+                        'Transferir',
+                    },
+                    text1 = 'Sacar',
+                    text2 = 'Depositar',
+                    text3 = 'Transferir',
+                    text4 = 'Valor',
+                    text5 = 'ID do Destinatário',
+                    text6 = 'Sua Conta'
+                }
+            },
+            statistics = {
+                title = 'Estatísticas',
+            }
+        },
+        savings_page = {
+            account = {
+                title = 'Conta Poupança',
+                subtext = 'Informações da sua poupança e ações disponíveis.',
+                personal_info = {
+                    cardNumber = 'Número do Cartão',
+                    date = 'Validade',
+                    cvv = 'CVV',
+                },
+            },
+            quick_transfer = {
+                title = 'Transferência Rápida',
+                subtext = 'Envie dinheiro instantaneamente para sua conta pessoal.',
+                transferOptions = {
+                    {
+                        text1 = 'Sacar',
+                        text2 = 'Valor',
+                        text3 = 'Sua Poupança'
+                    },
+                    {
+                        text1 = 'Depositar',
+                        text2 = 'Valor',
+                        text3 = 'Sua Poupança'
+                    }
+                }
+            },
+            savings_history = {
+                title = 'Histórico da Poupança',
+                subtext = 'Acompanhe o crescimento da sua poupança ao longo do tempo.',
+                daily = 'Diário',
+            },
+            transactions = {
+                title = 'Transações',
+                types = {
+                    text1 = 'Tipo',
+                    text2 = 'Valor',
+                    text3 = 'Status',
+                    text4 = 'Data',
+                }
+            },
+        }
+    }
+}
