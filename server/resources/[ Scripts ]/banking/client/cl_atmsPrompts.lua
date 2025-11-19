d763c7481aa943c7b97de2c83125200fbf3f1e5c
Reborn-Studios <allysonbedim99@gmail.com>
2025-11-19 19:21:21 -0300

    update(banking): atm com target

--- server/resources/[ Scripts ]/banking/client/cl_atmsPrompts.lua
+++ server/resources/[ Scripts ]/banking/client/cl_atmsPrompts.lua
@@ -6,38 +6,16 @@ local createdAtmPoints = {}
 local atmModels = Config.atmModels
 
 CreateThread(function()
-  while true do
-    for i = 1, #atmModels, 1 do
-      local playerPed = PlayerPedId()
-      local playerCoords = GetEntityCoords(playerPed)
-      local obj = GetClosestObjectOfType(playerCoords, 0.7, atmModels[i], false, false, false)
-      if obj ~= 0 then
-        local coords = GetEntityCoords(obj)
-        local existsNear = false
-        for j = 1, #createdAtmPoints, 1 do
-          if Vdist2(coords, createdAtmPoints[j]) < 2 then
-            existsNear = true
-            break
-          end
-        end
-        if not existsNear then
-          local point = CreatePoint({ coords = coords, radius = 2 })
-          point.Inside = function()
-            if IsControlJustPressed(0, 38) then
-              manageUI(true, "BANKINGVISABLITY", Callbacks.Await("fetchAccountData"), true)
-            end
-          end
-          point.OnEnter = function()
-            Config.drawText(Lang.client.atm_interact)
-          end
-          point.OnExit = function()
-            Config.hideText()
-            manageUI(false, "BANKINGVISABLITY", {}, false, true)
-          end
-          createdAtmPoints[#createdAtmPoints + 1] = coords
-        end
-      end
-    end
-    Wait(1000)
-  end
+  exports["ox_target"]:addModel(atmModels,{
+		{
+			label = "Abrir ATM",
+			icon = "fa-solid fa-money-bill-transfer",
+			event = "Banking:openAtm"
+		}
+	})
+end)
+
+RegisterNetEvent("Banking:openAtm")
+AddEventHandler("Banking:openAtm", function()
+  manageUI(true, "BANKINGVISABLITY", Callbacks.Await("fetchAccountData"), true)
 end)
