2f55a55cf1ea4ee803b782ed9c213504722f894c
Reborn-Studios <allysonbedim99@gmail.com>
2025-11-25 00:57:35 -0300

    update(loading): verificar valores antes de modificar

--- server/resources/[ System ]/loading/server/server.lua
+++ server/resources/[ System ]/loading/server/server.lua
@@ -1,18 +1,25 @@
 CreateThread(function ()
     Wait(1000)
+    local changedText = false
     local data = json.decode(LoadResourceFile(GetCurrentResourceName(), 'html/config.json'))
-    data["watermark"] = {
-        label = {
-            text = GlobalState['Basics']['ServerName'],
-            colorWordCount = 1
-        },
-        subHeading = "Loading Screen",
-        logo = GlobalState['Basics']['CityLogo']
-    }
+    if data["watermark"] and data["watermark"]["label"]?.text ~= GlobalState['Basics']['ServerName'] then
+        data["watermark"] = {
+            label = {
+                text = GlobalState['Basics']['ServerName'],
+                colorWordCount = 1
+            },
+            subHeading = "Loading Screen",
+            logo = GlobalState['Basics']['CityLogo']
+        }
+        changedText = true
+    end
     for k,v in pairs(data["socialHeaders"]) do
-        if v["type"] == "discord" then
+        if v["type"] == "discord" and data["socialHeaders"][k]["link"] ~= GlobalState['Basics']['Discord'] then
             data["socialHeaders"][k]["link"] = GlobalState['Basics']['Discord']
+            changedText = true
         end
     end
-    SaveResourceFile(GetCurrentResourceName(), 'html/config.json', json.encode(data, { indent = true }), -1)
+    if changedText then
+        SaveResourceFile(GetCurrentResourceName(), 'html/config.json', json.encode(data, { indent = true }), -1)
+    end
 end)
