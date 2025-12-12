09bf48b4d46c8182bf002484d085406ffaaf7b11
Reborn-Studios <allysonbedim99@gmail.com>
2025-12-12 12:55:30 -0300

    update(will_notifications): fxmanifest

--- server/resources/[ Reborn ]/will_notifications/fxmanifest.lua
+++ server/resources/[ Reborn ]/will_notifications/fxmanifest.lua
@@ -3,16 +3,15 @@ game 'gta5'
 lua54 'yes'
 
 client_scripts {
-    "@vrp/lib/utils.lua",
     'client-side/*.lua'
 }
 
 shared_scripts {
+    "@vrp/lib/utils.lua",
     'config.lua'
 }
 
 server_scripts {
-    "@vrp/lib/utils.lua",
     "server-side/server.lua"
 }
 
