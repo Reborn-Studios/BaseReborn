9e5c09338afc99444d96ea1b70b3ff69a21e430d
Reborn-Studios <allysonbedim99@gmail.com>
2025-12-19 00:38:13 -0300

    fix: lista de grupos

--- server/resources/[ Gerais ]/AdminControl/groups/server.lua
+++ server/resources/[ Gerais ]/AdminControl/groups/server.lua
@@ -8,9 +8,8 @@ RegisterServerEvent("AdminControl:setUserGroups",function (nuser_id, addGroups, 
         vRP.createWeebHook(Webhooks.webhookset,"```prolog\n[ID]: "..user_id.." \n[SETOU]: "..nuser_id.." \n [GROUP]: "..AddGroup.." "..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
     end
     for RemGroup, v in pairs(remGroups) do
-        vRP.removePermission(nuser_id, RemGroup)
+        vRP.removeUserGroup(nuser_id,RemGroup)
         TriggerClientEvent("Notify",source,"aviso","O cidadão foi retirado de " ..RemGroup.." ",5000)
-		vRP.execute("vRP/del_group",{ user_id = nuser_id, permiss = RemGroup })
         vRP.createWeebHook(Webhooks.webhookunset,"```prolog\n[ID]: "..user_id.." \n[RETIROU SET]: "..nuser_id.." \n [GROUP]: "..RemGroup.." "..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
     end
 end)
