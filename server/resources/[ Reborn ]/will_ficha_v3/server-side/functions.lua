0a21baaeb39e6f271c33f5b1fdf8e824ac1fa288
Reborn-Studios <allysonbedim99@gmail.com>
2026-03-05 13:41:06 -0300

    fix: veiculos de emprego nas garagens

--- server/resources/[ Reborn ]/will_ficha_v3/server-side/functions.lua
+++ server/resources/[ Reborn ]/will_ficha_v3/server-side/functions.lua
@@ -338,7 +338,7 @@ CreateThread(function()
     prepare("ficha/remove_arrest", "DELETE FROM will_ficha WHERE id = @id")
     prepare("ficha/get_vehicle_by_plate","SELECT user_id, vehicle, plate FROM "..Config.vehicle_db.." WHERE plate = @plate")
     
-    prepare("ficha/get_vehicles","SELECT * FROM "..Config.vehicle_db.." WHERE user_id = @user_id")
+    prepare("ficha/get_vehicles","SELECT * FROM "..Config.vehicle_db.." WHERE user_id = @user_id AND work = 'false'")
     prepare("vRP/add_group","INSERT INTO permissions(user_id,permiss) VALUES(@user_id,@permiss)")
     prepare("vRP/del_group","DELETE FROM permissions WHERE user_id = @user_id AND permiss = @permiss")
     prepare("vRP/get_specific_perm","SELECT * FROM permissions WHERE permiss = @permiss")
