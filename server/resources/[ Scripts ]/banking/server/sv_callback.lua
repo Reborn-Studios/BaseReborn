2e9c157aec751fff031db4b4ad4383cd289ff531
Reborn-Studios <allysonbedim99@gmail.com>
2026-04-09 22:14:45 -0300

    remove: banking

--- server/resources/[ Scripts ]/banking/server/sv_callback.lua
+++
@@ -1,9 +0,0 @@
-function registerServerCallback(name, handler)
-  local eventName = "__svBanking:callback:" .. name
-  RegisterNetEvent(eventName, function(...)
-    local src = source
-    local result = handler(src, ...)
-    TriggerClientEvent(eventName, src, result)
-  end)
-end
-RegisterCallback = registerServerCallback
